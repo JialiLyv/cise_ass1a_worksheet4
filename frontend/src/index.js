39a0d50e22db522c80e2bb97aa5b0e7f2ee8b3cf
Jiali Lu <jialilyv@gmail.com>
2022-04-08 14:44:15 +0800

    complete

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Main from "./App";
+import App from "./App"; //import into index.js file
+import "./index.css";
+
+ReactDOM.render(
+  <App/>, 
+  document.getElementById("root")
+);
